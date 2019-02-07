8ed802b8df6ce6c6696755f01adc42b86c058562
GrammySpaghetti <ryanchuray@me.com>
2019-02-07 13:48:39 -0600

    voting app v2 finished

--- voting-app/src/App.js
+++ voting-app/src/App.js
@@ -3,15 +3,35 @@ import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import Square from './Square';
 
-const App = (props) => {
-  return(
-    <div className="Constainer">
-      <Square name="React" />
-      <Square name="Vue" />
-      <Square name="Angular" />
-      <Square name="Ember" />
-    </div>
-  )
+// const App = (props) => {
+//   return(
+//     <div className="Constainer">
+//       <Square name="React" />
+//       <Square name="Vue" />
+//       <Square name="Angular" />
+//       <Square name="Ember" />
+//     </div>
+//   )
+// }
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return(
+      <div className="Constainer">
+        <h1>Vote For Your Favorite JS Library!</h1>
+        <Square name="React" />
+        <Square name="Vue" />
+        <Square name="Angular" />
+        <Square name="Ember" />
+      </div>
+    )
+  }
 }
 
 export default App
--- voting-app/src/Square.js
+++ voting-app/src/Square.js
@@ -3,16 +3,41 @@ import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 
 
-const Square = (props) => {
-  const { name } = props;
-  return (
-    <div className="col-lg-4 row border border-dark m-2 p-2" onClick={() => {alert('I voted for ' + name)}}>
-      <p className="col-8">{name}</p>
-      <button className="btn btn-info">Vote!</button>
-    </div>
-  )
-}
+// const Square = (props) => {
+//   const { name } = props;
+//   return (
+//     <div className="col-lg-4 row border border-dark m-2 p-2" onClick={() => {alert('I voted for ' + name)}}>
+//       <p className="col-8">{name}</p>
+//       <button className="btn btn-info">Vote!</button>
+//     </div>
+//   )
+// }
 
+class Square extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0,
+    }
+  }
+  updateCount = () => {
+    this.setState({count: this.state.count + 1})
+  }
+  render(){
+    const { name } = this.props;
+    return(
+      <div className="col-lg-4 row border border-dark m-2 p-2">
+        
+        <p className="col-8">{name} Received {this.state.count} Votes</p>
+        <button
+          className="btn btn-info"
+          onClick={this.updateCount}>
+          Vote!
+        </button>
+      </div>
+    )
+  }
+}
 
 
 export default Square
